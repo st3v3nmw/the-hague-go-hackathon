c317b5b3a9373adf4f3f8500a1c1ce08c3ed6808
Mitch Burton <mitch.burton@canonical.com>
2024-10-31 12:53:50 +0100

    GetThread takes client instead of creating one

--- mattermost.go
+++ mattermost.go
@@ -3,14 +3,10 @@ package main
 import (
 	"context"
 	"github.com/mattermost/mattermost/server/public/model"
-	"os"
 )
 
-func GetThread(postId string) ([]string, error) {
+func GetThread(client *model.Client4, postId string) ([]string, error) {
 	// Provides a list of post messages from the same thread as `postId`, in reverse order.
-	client := model.NewAPIv4Client("https://chat.canonical.com")
-	client.SetToken(os.Getenv("MM_AUTHTOKEN"))
-
 	postList, _, err := client.GetPostThread(context.Background(), postId, "", false)
 
 	if err != nil {
